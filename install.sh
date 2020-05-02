c8886f1406d723efca173181a4cd15f945b074c9
Alexandre Rebillard <haldrex.mail@gmail.com>
2020-05-02 10:42:11 -0400

    create scripts

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+if ! [ $(id -u) = 0 ]; then
+	echo 'This script must be run as root'
+	exit 1
+fi
+
+# apt-get caja-acions
+echo '[+]Installing caja-actions...'
+apt-get -y install caja-actions
+
+echo '[+]Copying file ...'
+
+# cp open_with_citrus.Rscript
+mkdir /home/citrus/local/share/bin > /dev/null 2>&1
+cp ./open_with_citrus.Rscript /home/citrus/.local/share/bin/
+
+# cp open_with_citrus.desktop & citrus icon
+mkdir /home/citrus/.local/share/file-manager/actions > /dev/null 2>&1
+cp ./open_with_citrus.desktop /home/citrus/.local/share/file-manager/actions/ 
+cp ./citrus.svg /home/citrus/.local/share/file-manager/actions/
+
+echo '[+]Done.'
+
